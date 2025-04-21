a3fe7a0af5c802b6f070e991a3b2f5dbca30ae26
Jihan Intani Nadhifah (Singapore) <DevOS01@courts.co.id>
2025-04-21 11:13:41 +0700

    Update delete button

--- product_module/urls.py
+++ product_module/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', product_list, name='product_list'),
     path('create/', product_create, name='product_create'),
     path('<int:pk>/edit/', product_update, name='product_update'),
-    path('<int:pk>/delete/', product_delete, name='product_delete'),
+    path('<int:pk>/delete/', product_delete, name='product_delete'),  
 ]
--- product_module/views.py
+++ product_module/views.py
@@ -39,10 +39,8 @@ def product_update(request, pk):
 @permission_required('product_module.delete_product', raise_exception=True)
 def product_delete(request, pk):
     product = get_object_or_404(Product, pk=pk)
-
     if request.method == "POST":
         product.delete()
         messages.success(request, f'Successfully deleted product: {product.name}')
-        return redirect('product_list')
-
-    return render(request, 'product_module/product_confirm_delete.html', {'product': product})
+    
+    return redirect('product_list')
